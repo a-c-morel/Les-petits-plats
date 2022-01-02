3b99b35b542d8f207f2583bb591e09e7804ba9ff
Adeline Claire MOREL <adelinemorel72@gmail.com>
2022-01-02 09:57:15 +0100

    fix: user can now use searchbar and then display filters under it when clicks in the lists

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 import { recipes } from './recipes.js';
 
+/*VARIABLES*/
+
 let recipesArray = recipes; //Array qui contient toutes les recettes (en tant qu'infos).
 let filteredRecipes = []; //Array qui contient les recette filtrées par une recherche
 let cardsArray = []; //Array qui contiendra les instances créées à partir de la classe Card (à créer), donc des objets. Cet Array devra se modifier en fonction de recipesArray, lui-même mis à jour par les recherches utilisateur, sans doute en liant l'id des recettes de recipesArray avec celui des objets de cardsArray.
 
 const mainElement = document.querySelector("main");
+const searchBar = document.querySelector("#searchbar");
+const searchbarError = document.querySelector("#error-msg-searchbar");//
+
+const tags = document.querySelector(".tags");
+
 
 /*FONCTIONS*/
 
@@ -21,7 +28,7 @@ function defaultDisplay(){
     }
 }
 
-function compareAndFilter(searchedLetters){
+function compareAndFilter(entry){
     filteredRecipes.splice(0, filteredRecipes.length);
     for (let recipe of recipesArray){
         let recipeTitle = recipe.name;
@@ -31,15 +38,12 @@ function compareAndFilter(searchedLetters){
             recipeIngredients.push(`${ingredient.toLowerCase()}`);
         });
         let recipeDescription = recipe.description;
-        if((recipeTitle.toLowerCase().includes(searchedLetters))||(recipeIngredients.toString().toLowerCase().includes(searchedLetters)) || (recipeDescription.toLowerCase().includes(searchedLetters)))
+        if((recipeTitle.toLowerCase().includes(entry))||(recipeIngredients.toString().toLowerCase().includes(entry)) || (recipeDescription.toLowerCase().includes(entry)))
         filteredRecipes.push(recipe);
     }
 }
 
 function filteredCardsDisplay(){
-    while (mainElement.firstChild) {
-        mainElement.removeChild(mainElement.firstChild);
-    }
     for (let filteredRecipe of filteredRecipes){
         let recipeTitle = filteredRecipe.name;
         let recipeDuration = filteredRecipe.time;
@@ -105,49 +109,6 @@ function hideList(btn, list){
     btn.classList.remove("w-50");
 }
 
-/*PAR DEFAUT, AFFICHAGE DE TOUTES LES CARTES*/
-
-defaultDisplay();
-
-/*RECUPERATION DES ARRAYS DE FILTRES (PAR DEFAUT = TOUS LES FILTRES)*/
-//boutons pour afficher les listes de filtres :
-displayFilters(recipesArray);
-
-/*ACTIONS DE L'UTILISATEUR ET LEURS EFFETS SUR L'INTERFACE*/
-//Cas d'utilisation : "l’utilisateur entre au moins 3 caractères dans la barre de recherche principale"
-//je récupère la barre de recherche et la zone pour le message d'erreur
-const searchBar = document.querySelector("#searchbar");
-const searchbarError = document.querySelector("#error-msg-searchbar");//
-
-searchBar.addEventListener("keyup", (e) => { //quand l'utilisateur entre des caractères dans la searchbar
-    const searchedLetters = e.target.value.toLowerCase(); //je convertis l'entrée utilisateur en minuscules, et je stocke cette donnée
-    if(searchedLetters.length>2){
-        compareAndFilter(searchedLetters);
-        filteredCardsDisplay();
-        displayFilters(filteredRecipes);
-    }
-});
-
-/****************************************************************************************************************/
-
-const tags = document.querySelector(".tags");
-
-const ingredientsTags = document.querySelectorAll(".ingredient-element");
-const appareilsTags = document.querySelectorAll(".appareil-element");
-const ustensilesTags = document.querySelectorAll(".ustensil-element");
-
-const ingredientsTagsArray = [];
-const appareilsTagsArray = [];
-const ustensilesTagsArray = [];
-
-const ingredientsClassName = "ingredient-tag";
-const appareilsClassName = "appareil-tag";
-const ustensilesClassName = "ustensil-tag";
-
-createTag(ingredientsTags, ingredientsTagsArray, ingredientsClassName);
-createTag(appareilsTags, appareilsTagsArray, appareilsClassName);
-createTag(ustensilesTags, ustensilesTagsArray, ustensilesClassName);
-
 function createTag(elements, arrayToFill, className){
     for (let element of elements){
         element.addEventListener('click', (e) => {
@@ -163,3 +124,58 @@ function createTag(elements, arrayToFill, className){
         });
     }
 }
+
+/*PAR DEFAUT, AFFICHAGE DE TOUTES LES CARTES*/
+
+defaultDisplay();
+
+/*RECUPERATION DES ARRAYS DE FILTRES (PAR DEFAUT = TOUS LES FILTRES)*/
+//boutons pour afficher les listes de filtres :
+displayFilters(recipesArray);
+
+/*ACTIONS DE L'UTILISATEUR ET LEURS EFFETS SUR L'INTERFACE*/
+//Cas d'utilisation : "l’utilisateur entre au moins 3 caractères dans la barre de recherche principale"
+searchBar.addEventListener("keyup", (e) => { //quand l'utilisateur entre des caractères dans la searchbar
+    const searchedLetters = e.target.value.toLowerCase(); //je convertis l'entrée utilisateur en minuscules, et je stocke cette donnée
+    if(searchedLetters.length>2){
+        compareAndFilter(searchedLetters);
+        while (mainElement.firstChild) {
+            mainElement.removeChild(mainElement.firstChild);
+        }
+        filteredCardsDisplay();
+        displayFilters(filteredRecipes);
+
+        const ingredientsTags = document.querySelectorAll(".ingredient-element");
+        const appareilsTags = document.querySelectorAll(".appareil-element");
+        const ustensilesTags = document.querySelectorAll(".ustensil-element");
+
+        const ingredientsTagsArray = []; //array qui contient les tags de type ingrédient (qui sont display en tant que filtres sous la searchbar)
+        const appareilsTagsArray = []; //array qui contient les tags de type appareil (qui sont display en tant que filtres sous la searchbar)
+        const ustensilesTagsArray = []; //array qui contient les tags de type ustensile (qui sont display en tant que filtres sous la searchbar)
+
+        const ingredientsClassName = "ingredient-tag";
+        const appareilsClassName = "appareil-tag";
+        const ustensilesClassName = "ustensil-tag";
+
+        createTag(ingredientsTags, ingredientsTagsArray, ingredientsClassName);
+        createTag(appareilsTags, appareilsTagsArray, appareilsClassName);
+        createTag(ustensilesTags, ustensilesTagsArray, ustensilesClassName);
+    }
+});
+
+//cas où l'utilisateur passe par les listes de filtres sans passer par la searchbar
+const ingredientsTags = document.querySelectorAll(".ingredient-element");
+const appareilsTags = document.querySelectorAll(".appareil-element");
+const ustensilesTags = document.querySelectorAll(".ustensil-element");
+
+const ingredientsTagsArray = []; //array qui contient les tags de type ingrédient (qui sont display en tant que filtres sous la searchbar)
+const appareilsTagsArray = []; //array qui contient les tags de type appareil (qui sont display en tant que filtres sous la searchbar)
+const ustensilesTagsArray = []; //array qui contient les tags de type ustensile (qui sont display en tant que filtres sous la searchbar)
+
+const ingredientsClassName = "ingredient-tag";
+const appareilsClassName = "appareil-tag";
+const ustensilesClassName = "ustensil-tag";
+
+createTag(ingredientsTags, ingredientsTagsArray, ingredientsClassName);
+createTag(appareilsTags, appareilsTagsArray, appareilsClassName);
+createTag(ustensilesTags, ustensilesTagsArray, ustensilesClassName);
